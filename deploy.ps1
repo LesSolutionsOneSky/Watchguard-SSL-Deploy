4d81cd0d1a4b26e34ab78eb78fd3c63aed14141c
mcajay01 <82970644+mcajay01@users.noreply.github.com>
2023-01-17 10:36:03 -0500

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,7 @@
-$DirectoryToCreate = "C:\temp"
-
 <# install Net Frame 4.8#>
 $DownloadNET = "https://github.com/LesSolutionsOneSky/Watchguard-SSL-Deploy/blob/main/WG-MVPN-SSL_12_7_2.exe"
 $SoftwareNETPath = "C:\temp\WG-MVPN-SSL_12_7_2.exe"
-if (-not (Test-Path -LiteralPath $DirectoryToCreate)) {
     mkdir "C:\temp"
-} Try {
     Write-Host "Downloading from: $($DownloadNET)"
     Write-Host "Downloading to:   $($SoftwareNETPath)"
         $WebClient = New-Object System.Net.WebClient
@@ -13,6 +9,3 @@ if (-not (Test-Path -LiteralPath $DirectoryToCreate)) {
     Write-Host "Download Complete"
 $process = (Start-Process -FilePath $SoftwareNETPath -ArgumentList "/silent /verysilent" -Wait -Verb RunAs -PassThru)
 Write-Host -Fore Red "Errorcode: " $process.ExitCode
-} catch {
-    Write-Host "Error in creating temp Folder! Error: " $process.ExitCode
-}
