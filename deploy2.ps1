7bf9e99cc03f612069b14048c06353b8dc116383
mcajay01 <82970644+mcajay01@users.noreply.github.com>
2023-01-20 08:48:50 -0500

    Update deploy2.ps1

--- deploy2.ps1
+++ deploy2.ps1
@@ -1,4 +1,10 @@
-Run 'certutil -addstore -f "TrustedPublisher" OpenVPN.cer' Show Waitdown Task:1
+taskkill /IM "wgsslvpnc.exe" /F
+
+"C:\Program Files (x86)\WatchGuard\WatchGuard Mobile VPN with SSL\unins000.exe" /VERYSILENT /NORESTART
+
+certutil -addstore -f TrustedPublisher trustedpublisher1.cer
+
+certutil -addstore -f TrustedPublisher trustedpublisher2.cer
 
 $DownloadNET = "https://github.com/LesSolutionsOneSky/Watchguard-SSL-Deploy/raw/main/WG-MVPN-SSL_12_7_2.exe"
 $SoftwareNETPath = "C:\temp\WG-MVPN-SSL_12_7_2.exe"
